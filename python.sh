43fbc55d046d438f815811892abdc5ff4d2fe923
Lucas Veras <lucasdsveras@gmail.com>
2020-04-15 11:09:21 +0100

    Initial commit

---
+++ python.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Installing python libraries..."
+
+# Upgrade pip
+pip3 install --upgrade pip
+
+# Install libraries
+pip3 install numpy
+pip3 install scipy
+pip3 install astropy
+pip3 install pandas
+pip3 install dask[complete]
+pip3 install ipython
+pip3 install jupyter
