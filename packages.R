a6d82526328bffa4cc99b0e0e36ad0e9d61c4e97
Lucas Veras <lucasdsveras@gmail.com>
2020-05-15 16:32:44 +0100

    Add packages

--- packages.R
+++ packages.R
@@ -1,17 +1,12 @@
-# Dev packages
-install.packages(c(
-  "devtools", "roxygen2", "testthat", "knitr", "shiny"
-  ),
-  repos = "http://cran.us.r-project.org")
-)
-
-# Other packages
 packages <- c(
+  # Dev
+  "devtools", "roxygen2", "testthat", "knitr", "shiny",
+  "microbenchmark", "sloop",
   # Main
   "tidyverse", "tidymodels", "here", "Rcpp", "RSQlite",
-  "cowplot", "janitor", "lobstr",
+  "cowplot", "janitor", "lobstr", "data.table", "fst",
   # Stats
-  "nlme", "lme4", "emmeans", "car",
+  "nlme", "lme4", "emmeans", "car", "MASS",
   # Accelerometry
   "PhysicalActivity", "GGIR"
 )
