efe48e197563db06a018cfa8335886372e43eb5c
Lucas <lucasdsveras@gmail.com>
2021-09-15 17:59:40 -0300

    Update R packages

--- packages.R
+++ packages.R
@@ -10,11 +10,13 @@ packages <- c(
   "tidyverse", "tidymodels", "here", "Rcpp", "RSQlite",
   "patchwork", "ragg", "janitor", "lobstr", "data.table", "fst",
   # Stats
-  "nlme", "lme4", "lmerTest", "emmeans", "car", "MASS",
+  "nlme", "lme4", "lmerTest", "emmeans", "car", "MASS", "psych"
   # Accelerometry
   "PhysicalActivity", "GGIR",
   # Github
-  "THLfi/read.gt3x", "jalvesaq/colorout", "rileytwo/darkstudio"
+  "THLfi/read.gt3x", "jalvesaq/colorout", "rileytwo/darkstudio",
+  # Mine
+  "lvmisc", "impactr"
 )
 
 pak::pkg_install(packages)
