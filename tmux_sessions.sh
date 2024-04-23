309a9adbc4f92cacdcb9eb5dd49283c0b0a6020a
Lucas <lucasdsveras@gmail.com>
2024-04-23 17:59:39 -0300

    Fix a bug in which a session would only be created if already in one

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -10,16 +10,13 @@ if [[ -z $selected ]]; then
   exit 0
 fi
 
-selected_name=$(basename $selected | tr . _)
-tmux_running=$(pgrep tmux)
+selected_name=$(basename "$selected" | tr . _)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s $selected_name -c $selected
-  exit 0
+if [[ -z $TMUX ]]; then
+  tmux new-session -s "$selected_name" -c "$selected"
+else
+  if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
+  fi
+  tmux switch-client -t "$selected_name"
 fi
-
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-  tmux new-session -ds $selected_name -c $selected
-fi
-
-tmux switch-client -t $selected_name
