a474d3d05781551e01c17bca44e82447f72c21b4
Lucas <lucasdsveras@gmail.com>
2024-04-23 17:59:56 -0300

    Delete files

--- clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Cloning repositories..."
-
-# Add command to clone below:
--- install.sh
+++ install.sh
@@ -14,9 +14,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Clone Github repositories
-./clone.sh
-
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
