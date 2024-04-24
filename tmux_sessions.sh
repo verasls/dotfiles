88b8442e476ecd540ac6a6ace46ab9bca9008ca2
Lucas <lucasdsveras@gmail.com>
2024-04-24 00:05:39 -0300

    Fix a bug in which you could not switch to an existing session when calling from outside a tmux session

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -12,11 +12,17 @@ fi
 
 selected_name=$(basename "$selected" | tr . _)
 
-if [[ -z $TMUX ]]; then
-  tmux new-session -s "$selected_name" -c "$selected"
-else
-  if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
+if tmux has-session -t="$selected_name" 2> /dev/null; then
+  if [[ -n $TMUX ]]; then
+    tmux switch-client -t "$selected_name"
+  else
+    tmux attach-session -t "$selected_name"
   fi
-  tmux switch-client -t "$selected_name"
+else
+  if [[ -z $TMUX ]]; then
+    tmux new-session -s "$selected_name" -c "$selected"
+  else
+    tmux new-session -ds "$selected_name" -c "$selected"
+    tmux switch-client -t "$selected_name"
+  fi
 fi
