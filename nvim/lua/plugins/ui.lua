f6f730ca7cb356808535f754a111b64e97b876d0
Lucas <lucasdsveras@gmail.com>
2024-04-25 00:00:48 -0300

    Add noice.nvim plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -32,5 +32,38 @@ return {
     config = function()
       require("ibl").setup()
     end
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+        views = {
+          cmdline_popup = {
+            position = {
+              row = "40%",
+              column = "50%",
+            }
+          }
+        }
+      })
+    end
   }
 }
