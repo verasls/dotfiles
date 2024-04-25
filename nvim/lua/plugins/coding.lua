b822bdfe101df443ed6daef3fde895357685c938
Lucas <lucasdsveras@gmail.com>
2024-04-25 00:46:32 -0300

    Config nvim to handle git better

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -31,20 +31,5 @@ return {
         end,
       },
     },
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add          = { text = '┃' },
-          change       = { text = '┃' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked    = { text = '┆' },
-        }
-      })
-    end
   }
 }
---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "tpope/vim-fugitive"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        }
+      })
+
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
+    end
+  }
+}
