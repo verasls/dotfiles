3712066bd032a2e649540b65ef3c2310d3da4346
Lucas <lucasdsveras@gmail.com>
2024-05-09 13:47:14 -0300

    Change remap for git blame

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -17,7 +17,7 @@ return {
       })
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true })
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
     end
   }
 }
