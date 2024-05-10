c86ce3e81e343c1cbbd01f5383ac56586d0a39e7
Lucas <lucasdsveras@gmail.com>
2024-05-09 21:31:25 -0300

    Add obsidian.nvim plugin

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,65 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter"
+  },
+  config = function()
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/Projects/_notes",
+        }
+      },
+      daily_notes = {
+        folder = "Periodic"
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>k"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        ["<CR>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        }
+      },
+      new_notes_location = "zzz",
+      wiki_link_func = "use_alias_only",
+      disable_frontmatter = true,
+      follow_url_func = function(url)
+        vim.fn.jobstart({"open", url})
+      end,
+      templates = { folder = "Templates" },
+      picker = {
+        name = "telescope.nvim",
+        mappings = {
+          new = "<C-x>",
+          insert_link = "<C-l>",
+        },
+      },
+      open_notes_in = "current",
+      ui = {}
+    })
+  end
+}
