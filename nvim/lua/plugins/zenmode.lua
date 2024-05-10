868ad155641a9ba20e2b7da5dabdea082bd0d47e
Lucas <lucasdsveras@gmail.com>
2024-05-09 22:53:01 -0300

    Update zen-mode options

--- nvim/lua/plugins/zenmode.lua
+++ nvim/lua/plugins/zenmode.lua
@@ -2,16 +2,17 @@ return {
   "folke/zen-mode.nvim",
   config = function()
     vim.keymap.set("n", "<leader>z", function()
-      require("zen-mode").setup {
-        window = {
-          width = 120,
-          options = {}
+      require("zen-mode").toggle({
+        plugins = {
+          twilight = { enabled = false },
+          gitsigns = { enabled = true },
+          tmux = { enabled = true },
+          alacritty = {
+            enabled = true,
+            font = "14",
+          },
         },
-      }
-      require("zen-mode").toggle()
-      vim.wo.wrap = true
-      vim.wo.number = true
-      vim.wo.rnu = true
+      })
     end)
   end
 }
