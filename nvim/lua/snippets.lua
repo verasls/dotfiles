c59a955aaab1b289674a6486e4475825b24ae24d
Lucas <lucasdsveras@gmail.com>
2025-05-27 16:07:57 -0500

    add more snippets

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -3,6 +3,44 @@ local ls = require 'luasnip'
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
+
+-- R code section
+
+ls.add_snippets('r', {
+  s('sect', {
+    t '# ',
+    i(1, 'SECT_NAME'),
+    t ' ',
+    f(function(args)
+      local sect_name = args[1][1]
+      local total_len = 80
+      local prefix = '# '
+      local space = ' '
+      local dash_count = total_len - (#prefix + #sect_name + #space)
+      if dash_count < 0 then
+        dash_count = 0
+      end
+      return string.rep('-', dash_count)
+    end, { 1 }),
+  }),
+})
+
+-- R assignment operator
+ls.add_snippets('r', {
+  s(',,', {
+    t '<- ',
+    i(0),
+  }),
+})
+
+-- R pipe operator
+ls.add_snippets('r', {
+  s('>>', {
+    t '|>',
+    i(0),
+  }),
+})
 
 -- Quarto R code block
 ls.add_snippets('all', {
