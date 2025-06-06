c22973211255eb7ba27cec54b7b46ff8c2ccb706
Lucas Veras <veraslucas@ACNC10361m.local>
2025-06-05 22:12:49 -0500

    update installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+set -e
 
 echo "Setting up your Mac..."
 
+# Define the dotfiles directory path
+DOTFILES="$HOME/projects/.dotfiles"
+
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if ! command -v brew >/dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -15,50 +19,42 @@ brew tap homebrew/bundle
 brew bundle
 
 # Zsh
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Git
-rm -rf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-
-rm -rf $HOME/.gitignore_global
-ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
-
-# Mackup
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+rm -f "$HOME/.zshrc"
+ln -s "$DOTFILES/.zshrc" "$HOME/.zshrc"
 
 # Neovim
-rm -rf $HOME/.config/nvim
-ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
+rm -rf "$HOME/.config/nvim"
+mkdir -p "$HOME/.config"
+ln -s "$DOTFILES/nvim" "$HOME/.config/nvim"
 
 # Ghostty
-rm -rf $HOME/.config/ghostty/config
-if [ ! -d "$HOME/.config/ghostty" ]; then
-  mkdir -p "$HOME/.config/ghostty"
-fi
-ln -s $HOME/.dotfiles/ghostty-config $HOME/.config/ghostty/config
+rm -f "$HOME/.config/ghostty/config"
+mkdir -p "$HOME/.config/ghostty"
+ln -s "$DOTFILES/ghostty-config" "$HOME/.config/ghostty/config"
 
 # Tmux
-rm -rf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+rm -f "$HOME/.tmux.conf"
+ln -s "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+# Install tmux plugin manager (tpm) if not already installed
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
+# Reload tmux configuration to apply changes
+tmux source-file "$HOME/.tmux.conf" || echo "tmux not running, skipping reload"
 
 # Starship
-rm -rf $HOME/.config/starship.toml
-ln -s $HOME/.dotfiles/starship.toml $HOME/.config/starship.toml
-
-# Fzf
-rm -rf $HOME/.fzf.zsh
-ln -s $HOME/.dotfiles/.fzf.zsh $HOME/.fzf.zsh
+rm -f "$HOME/.config/starship.toml"
+mkdir -p "$HOME/.config"
+ln -s "$DOTFILES/starship.toml" "$HOME/.config/starship.toml"
 
 # R
-rm -rf $HOME/.Rprofile
-ln -s $HOME/.dotfiles/.Rprofile $HOME/.Rprofile
+rm -f "$HOME/.Rprofile"
+ln -s "$DOTFILES/.Rprofile" "$HOME/.Rprofile"
 
 # Aerospace
-mkdir -p $HOME/.config/aerospace
-rm -rf $HOME/.config/aerospace/aerospace.toml
-ln -s $HOME/.dotfiles/aerospace.toml $HOME/.config/aerospace/aerospace.toml
+mkdir -p "$HOME/.config/aerospace"
+rm -f "$HOME/.config/aerospace/aerospace.toml"
+ln -s "$DOTFILES/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
 
 # Set macOS preferences
-source .macos
+source "$DOTFILES/.macos"
