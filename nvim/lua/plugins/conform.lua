258944c375eb0849b449a84f0c7948c61b467ac5
Lucas Veras <lucasdsveras@gmail.com>
2025-06-17 11:31:26 -0500

    config code formatter for quarto documents

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         r = { 'air' },
+        quarto = { 'injected' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
